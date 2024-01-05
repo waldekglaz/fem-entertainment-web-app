8ac8f6849f39a02fb72e749a72a2b81acdc90ccd
Waldemar Glaz <waldekglaz@gmail.com>
2024-01-05 22:39:27 +0100

    active Navlink styles fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { useState, ChangeEvent } from 'react'
-import PageWrapper from '../components/PageWrapper'
+import { useState, ChangeEvent } from "react";
+import PageWrapper from "../components/PageWrapper";
 import {
   SectionTitle,
   TrendingCard,
@@ -7,38 +7,38 @@ import {
   StandardCardContainer,
   Search,
   NoItems,
-} from '../components'
-import useStore from '../store'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import { Movie } from '../store'
+} from "../components";
+import useStore from "../store";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Movie } from "../store";
 
 function Home() {
-  const { movies } = useStore()
-  const trendingMovies = movies.filter((movie) => movie.isTrending)
+  const { movies } = useStore();
+  const trendingMovies = movies.filter((movie) => movie.isTrending);
 
-  const [userInput, setUserInput] = useState('')
+  const [userInput, setUserInput] = useState("");
 
-  const [searchedMovies, setSearchedMovies] = useState<Movie[]>([])
+  const [searchedMovies, setSearchedMovies] = useState<Movie[]>([]);
 
   const onSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value
-    setUserInput(inputValue)
+    const inputValue = e.target.value;
+    setUserInput(inputValue);
 
     const searchedResults = movies.filter((movie) =>
-      movie.title.toLowerCase().includes(inputValue.toLowerCase()),
-    )
-    setSearchedMovies(searchedResults)
-  }
+      movie.title.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setSearchedMovies(searchedResults);
+  };
 
-  const displayedMovies = userInput ? searchedMovies : movies
+  const displayedMovies = userInput ? searchedMovies : movies;
 
   return (
     <PageWrapper>
       <Search onChange={onSearch} value={userInput} />
       <SectionTitle text="Trending" />
       <Swiper
-        slidesPerView={1.2}
+        slidesPerView={1.6}
         breakpoints={{
           768: {
             slidesPerView: 1.3,
@@ -48,13 +48,14 @@ function Home() {
             slidesPerView: 1.7,
           },
           1440: {
-            slidesPerView: 2.9,
+            slidesPerView: 2.5,
           },
           1920: {
             slidesPerView: 3.3,
             spaceBetween: 16,
           },
-        }}>
+        }}
+      >
         {trendingMovies.map((movie, index) => (
           <SwiperSlide className="swiper">
             <TrendingCard
@@ -87,7 +88,7 @@ function Home() {
         <NoItems message="No results to match your search" />
       )}
     </PageWrapper>
-  )
+  );
 }
 
-export default Home
+export default Home;
